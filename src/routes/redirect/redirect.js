bf571f2ffe9d2fe926539648372afd2780166559
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-02-12 10:26:57 +1300

    Update: Added all the code to make the redirects works

--- src/routes/redirect/redirect.js
+++ src/routes/redirect/redirect.js
@@ -3,6 +3,28 @@ const router = express.Router();
 
 const pool = require("../../core/database/pool");
 
-router.get("/", async (_, res) => {});
+router.get("/:name", async (req, res) => {
+  let name = req.params.name;
+
+  let query = {
+    text: "SELECT url from redirects where name=$1",
+    values: [name],
+  };
+
+  let sqlRes;
+
+  try {
+    sqlRes = await pool.query(query);
+  } catch (err) {
+    return res.send({ detail: err.stack });
+  }
+
+  if (sqlRes.rowCount === 0)
+    return res.send({ detail: "This redirect does not exist" });
+
+  let url = sqlRes.rows[0].url;
+
+  return res.redirect(url);
+});
 
 module.exports = router;
