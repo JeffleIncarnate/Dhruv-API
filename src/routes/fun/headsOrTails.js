95fc19f974173cf0c4443b6592ff8d658b755804
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-02-11 19:59:48 +1300

    Update: Update the endpoint for headsOrTails

--- src/routes/fun/headsOrTails.js
+++ src/routes/fun/headsOrTails.js
@@ -1,19 +1,8 @@
-/**
- * @swagger
- * /yes:
- *  get:
- *   tag:
- *    - Yes
- *    description: Y E S
- *    responses:
- *      200:
- *        description: YES YES YES
- */
 const express = require("express");
 const router = express.Router();
 
 const headsOrTails = require("../../core/utilities/random").headsOrTails;
 
-router.get("/", (req, res) => res.send(headsOrTails()));
+router.get("/", (req, res) => res.send({ detail: headsOrTails() }));
 
 module.exports = router;
