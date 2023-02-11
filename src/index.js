76c876246d99d4e741c1a91529558d1cf9759604
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-02-11 20:00:31 +1300

    Update: Added all the endpoints from the routes/ endpoint

--- src/index.js
+++ src/index.js
@@ -1,55 +1,19 @@
 const express = require("express");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
-
-const port = 3000 || process.env.port;
 
 const app = express();
-
-// Swagger -- Start
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Dhruv API",
-      version: "0.1.0",
-      description:
-        "This is the new Dhruv API, it is a general purpose API and can do many of the things",
-      license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
-      },
-      contact: {
-        name: "Twitter",
-        url: "https://twitter.com/RayatDhruv",
-      },
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
-
-const specs = swaggerJsdoc(options);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
-// Swagger -- End
+const port = 3000 || process.env.port;
 
 app.get("/", (req, res) => {
-  return res.redirect(`${req.protocol}://${req.headers.host}/docs`);
+  return res.send({ detail: "Welcome to the Dhruv API" });
 });
 
-app.get("/yes", (req, res) => {
-  return res.sendStatus(200);
-});
+// Require Routes -- RANDOM
+const headsOrTails = require("./routes/fun/headsOrTails");
+const randomDare = require("./routes/fun/randomDare");
 
-// Require Routes
-const headsOrTails = require("./routes/fun/headsOrTails.js");
-
-// User Routes
-app.use("/headsOrTails", headsOrTails);
+// Use Routes -- RANDOM
+app.use("/random/headsOrTails", headsOrTails);
+app.use("/random/randomDare", randomDare);
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:3000`);
