2a0b45cde0ef9f12adf4c7e1de85330d7b7b4e36
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-02-14 11:45:19 +1300

    Update: Serving the new endpoints

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   return res.send({ detail: "Welcome to the Dhruv API" });
 });
 
+app.all("*", (_, res) => {
+  return res.send({ detail: "This endpoint does not exist" });
+});
+
 // Require Routes -- RANDOM
 const headsOrTails = require("./routes/fun/headsOrTails");
 const randomDare = require("./routes/fun/randomDare");
@@ -19,6 +23,9 @@ const getRedirects = require("./routes/redirect/getRedirects");
 const redirect = require("./routes/redirect/redirect");
 const deleteRedirect = require("./routes/redirect/deleteRedirect");
 
+// Require Routes -- IMAGES
+const qrCode = require("./routes/images/qrCode");
+
 // Use Routes -- RANDOM
 app.use("/random/headsOrTails", headsOrTails);
 app.use("/random/randomDare", randomDare);
@@ -29,6 +36,9 @@ app.use("/redirect/makeRedirect", makeRedirect);
 app.use("/redirect/getRedirects", getRedirects);
 app.use("/r", redirect);
 app.use("/redirect/deleteRedirect", deleteRedirect);
+
+// Use Routes -- IMAGES
+app.use("/images/qrCode", qrCode);
 
 app.listen(port, () => {
   console.log(`Listening at on port ${port}`);
